5e7a501038af44783a0de1ac8447e402a845f67d
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2025-01-23 00:55:00 +0400

    homework_14.2 done

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,8 @@
+from unittest.mock import patch
+
+from src.product import Product
+
+
 def test_product_init(first_product, second_product):
     assert first_product.name == "Samsung Galaxy S23 Ultra"
     assert first_product.description == "256GB, Серый цвет, 200MP камера"
@@ -8,3 +13,34 @@ def test_product_init(first_product, second_product):
     assert second_product.description == "512GB, Gray space"
     assert second_product.price == 210000.0
     assert second_product.quantity == 8
+
+
+def test_product_new_product(data):
+    result = Product.new_product(data)
+    assert result.name == "Samsung Galaxy C23 Ultra"
+    assert result.description == "256GB, Серый цвет, 200MP камера"
+    assert result.price == 185000.0
+
+
+def test_product_new_product_with_list(data, list_products):
+    result = Product.new_product(data, list_products)
+    assert result.name == "Samsung Galaxy C23 Ultra"
+    assert result.quantity == 10
+    assert result.price == 185000.0
+
+
+def test_product_price_setter_new_price(first_product):
+    first_product.price = 185000
+    assert first_product.price == 185000
+
+
+def test_product_price_setter_min_price(first_product):
+    with patch("builtins.input", return_value="y"):
+        first_product.price = 800
+        assert first_product.price == 800
+
+
+def test_product_price_setter_negative_price(capsys, first_product):
+    first_product.price = -100
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
